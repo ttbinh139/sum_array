b69b35e393c307cf797adc42ccaf42d7f7d31b8e
ttbinh139 <ttbinh139@gmail.com>
2022-05-07 02:07:04 +0000

    Implement function calculate total number of multi nested array

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let total = 0;
+  array.forEach(element => {
+    if (Array.isArray(element)) {
+      total += sumItems(element);
+    } else {
+      total += element;
+    }
+  });
+  return total;
 }
 
 module.exports = sumItems;
